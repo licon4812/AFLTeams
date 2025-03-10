aa8652ab38ca1a54f4a29b48e5aaecb9ced9296f
James Alickolli <jalickolli@gmail.com>
2025-03-10 13:17:40 +1100

    Created class library and test project

---
+++ src/AFLTeams.Tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using EnumFastToStringGenerated;
+
+namespace AFLTeams.Tests;
+
+public class Tests
+{
+    [Test]
+    [MethodDataSource(nameof(DataSource))]
+    public async Task AFLTeamIsPresent(string team)
+    {
+        await Assert.That(AFLTeamEnumExtensions.DisplayNamesDictionary.Values).Contains(team);
+    }
+
+
+    public static IEnumerable<string> DataSource()
+    {
+        return AFLTeamEnumExtensions.DisplayNamesDictionary.Values;
+    }
+}
---
+++ src/AFLTeams/AFLTeam.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EnumFastToStringGenerated;
+
+namespace AFLTeams
+{
+    [EnumGenerator]
+    public enum AFLTeam
+    {
+        [Display(Name = "Adelaide Crows")]
+        AdelaideCrows,
+
+        [Display(Name = "Brisbane Lions")]
+        BrisbaneLions,
+
+        [Display(Name = "Carlton")]
+        Carlton,
+
+        [Display(Name = "Collingwood")]
+        Collingwood,
+
+        [Display(Name = "Essendon")]
+        Essendon,
+
+        [Display(Name = "Fremantle")]
+        Fremantle,
+
+        [Display(Name = "Geelong")]
+        Geelong,
+
+        [Display(Name = "Gold Coast Suns")]
+        GoldCoastSuns,
+
+        [Display(Name = "GWS Giants")]
+        GWSGiants,
+
+        [Display(Name = "Hawthorn")]
+        Hawthorn,
+
+        [Display(Name = "Melbourne")]
+        Melbourne,
+
+        [Display(Name = "North Melbourne")]
+        NorthMelbourne,
+
+        [Display(Name = "Port Adelaide")]
+        PortAdelaide,
+
+        [Display(Name = "Richmond")]
+        Richmond,
+
+        [Display(Name = "St Kilda")]
+        StKilda,
+
+        [Display(Name = "Sydney Swans")]
+        SydneySwans,
+
+        [Display(Name = "West Coast Eagles")]
+        WestCoastEagles,
+
+        [Display(Name = "Western Bulldogs")]
+        WesternBulldogs
+    }
+}
